0c1f20a3e690414b768309a4f6a770fd7b0697e6
GuilhermePavani <guipavani429@gmail.com>
2025-09-18 10:47:51 -0300

    instalacao tailwind e teste de rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Guilherme', function() {
+    return 'olá Guilherme';
+});
+
+Route::get('/filmes/{filme}', function($filme) {
+    return 'Acessando dados do filme: '.$filme;
+});
+
+Route::view('/conheca','sobre');
