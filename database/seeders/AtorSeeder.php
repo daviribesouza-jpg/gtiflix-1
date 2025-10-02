634a33ffd121667581717d49d034cb17d33342c2
GuilhermePavani <guipavani429@gmail.com>
2025-10-02 10:55:59 -0300

    Criacao de Models e Seeders ator e genero

---
+++ database/seeders/AtorSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AtorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('atores')->insert(
+            ['nome'=>"Wagner Moura", 'descricao'=>"Ator Brasileiro", 'nacionalidade_id'=>1],
+            ['nome'=>"Leonardo DiCaprio", 'descricao'=>"Ator Estadunidense", 'nacionalidade_id'=>2]
+            );
+    }
+}
